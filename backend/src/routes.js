ccc76de8284a5d15f1ff492ba3c5c8cecdfac63c
kaio <kaiorei897@gmail.com>
2020-04-04 12:45:09 -0300

    Dev_Radar

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+
+const routes = Router();
+
+routes.get('/devs', DevController.index);
+routes.post('/devs',DevController.store );
+routes.get('/search',SearchController.index);
+
+module.exports = routes;
+
+
+//Métodos HTTP: GET, POST, DELETE
+
+//Tipos de parametros:
+
+//Query Params req.query (filtros,ordenação, paginação)
+//route params: request.params (Identificar um recurso na alteração ou remoção)
+//Body:request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (não-relacional)
